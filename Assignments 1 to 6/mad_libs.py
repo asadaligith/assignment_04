f1eed489fd7afd08149fff91bab0ff21647c8709
asadaligith <asad2747982@gmail.com>
2025-03-29 03:19:51 +0500

    updated

---
+++ Assignments 1 to 6/mad_libs.py
@@ -0,0 +1,13 @@
+print("\nWelcome to Madlibs assaignment\n")
+
+def main():
+    name = input("Enter Your Name: ")
+    age = int(input("Enter Your age: "))
+    institute = input("Enter your Centar: ")
+    language = input("learning Language: ")
+
+
+    print(f"\nMy name is {name} I am {age} Years old I have learning {language} programming language from {institute}\n")
+
+if __name__ == '__main__':
+    main()
---
+++ Assignments 1 to 6/numberGuess.py
@@ -0,0 +1,33 @@
+import random
+
+def number_guessing_game():
+    print("Number Guessing Game")
+    try:
+        user_input = int(input("Choose a Number between 1 to 50: "))
+        if user_input < 1 or user_input > 50:
+            print("Choose a number only between 1 to 50")
+            
+    except ValueError:
+        print("Invalid Input! Choose a number between 1 to 50")
+        
+    
+    low, high = 1, 50
+    attempts = 0
+    
+    while True:
+        computer_guess = random.randint(low, high)
+        attempts += 1
+        print(f"Computer guesses: {computer_guess}")
+        user_reply = input("Tell if the number is Correct, High, or Low: ").strip().lower()
+        
+        if user_reply == "correct":
+            print(f"Computer guessed the correct number {computer_guess} in {attempts} attempts!")
+            break
+        elif user_reply == "high":
+            high = computer_guess - 1
+        elif user_reply == "low":
+            low = computer_guess + 1
+        else:
+            print("Invalid input! Please enter 'Correct', 'High', or 'Low'.")
+
+number_guessing_game()
